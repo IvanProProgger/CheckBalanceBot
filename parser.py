c4db28fa3bfa3d51d0163da7b2ba396a50497e24
ishelukhin <ishelukhin@tennisi.it>
2023-12-10 11:38:41 +0300

    some fixes

--- parser.py
+++ parser.py
@@ -1,11 +1,17 @@
 import asyncio
+
+import config
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from datetime import date, datetime, timedelta
+from PIL import Image, ImageOps, ImageEnhance
+import pytesseract
+import cv2
 
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 message_date = datetime.today().date() - timedelta(days=1)
 
 class Parser:
@@ -64,13 +70,61 @@ class Parser:
         wait = WebDriverWait(self.driver, 15).until_not(
             EC.text_to_be_present_in_element((By.CSS_SELECTOR, 'tbody :first-child td'), 'Запись загружается...')
         )
-        self.driver.implicitly_wait(2)
         date_elem = self.driver.find_element(By.CSS_SELECTOR, 'tbody :first-child td').text
         if date_elem == "Отсутствуют данные":
             return False
         sms_date = datetime.strptime(date_elem, '%d.%m.%Y %H:%M').date()
         now = datetime.now().date()
         return sms_date == now
+
+    async def get_captcha_symbols(self):
+        text = ''
+        while len(text) != 4:
+            self.driver.get("https://lcab.smsprofi.ru/cabinet/login")
+            wait = WebDriverWait(self.driver, 15).until(
+                EC.visibility_of_element_located((By.CSS_SELECTOR, '[src*="/cabinet/api/clientOrGuest"]'))
+            )
+            captcha_element = self.driver.find_element(By.CSS_SELECTOR, '[src*="/cabinet/api/clientOrGuest"]')
+            captcha_element.screenshot("captcha.png")
+            captcha_image = Image.open("captcha.png")
+            captcha_image = captcha_image.resize((300, 100))
+            enhancer = ImageEnhance.Contrast(captcha_image)
+            captcha_image = enhancer.enhance(5) # Увеличение контрастности в 2 раза
+            captcha_image.save("captcha.png")
+            screenshot = cv2.imread("captcha.png", cv2.IMREAD_GRAYSCALE)
+            blurred = cv2.GaussianBlur(screenshot, (5, 5), 0)
+            _, thresholded = cv2.threshold(blurred, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+            cv2.imwrite("captcha.png", thresholded)
+            text = Parser.decrypt("captcha.png")
+
+        return text
+
+    async def login_smsprofi(self, text):
+        login = self.driver.find_element(By.ID, 'login')
+        password = self.driver.find_element(By.ID, 'password')
+        captcha = self.driver.find_element(By.CSS_SELECTOR, '[class*="mr-2"] input')
+        login.send_keys('yr')
+        password.send_keys('T?VxEZm#KqMRw6C+6>#Co!YU')
+        captcha.send_keys(text)
+        enter = self.driver.find_element(By.CSS_SELECTOR,
+                                         '[class="el-form el-form--label-top"] [type="submit"]')
+        enter.click()
+        try:
+            wait = WebDriverWait(self.driver, 5).until(
+                EC.visibility_of_element_located((By.CSS_SELECTOR, '[class*="org-descr"] span:first-child'))
+            )
+            balance = self.driver.find_element(By.CSS_SELECTOR, '[class*="org-descr"] span:first-child').text
+            return float(balance)
+        except:
+            self.driver.refresh()
+            return False
+
+    @staticmethod
+    def decrypt(image):
+        data = pytesseract.image_to_string(image, config='--psm 10 --oem 3 -c tessedit_char_whitelist=0123456789')
+        text = ''.join(data.split())
+        text = ''.join(text.split('/n'))
+        return text
 
     async def quit(self):
         self.driver.quit()
